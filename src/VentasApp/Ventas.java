98e2a412b3623d231519ca1323e6a8e4af0ead27
yarevalo <yarevalo@Nei_aux_sis02.alcanos.net>
2018-10-31 19:26:09 -0500

    Se inicia con la creación del formulario Ventas

---
+++ src/VentasApp/Ventas.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventasApp;
+
+/**
+ *
+ * @author yarevalo
+ */
+public interface Ventas extends VentasOperations, org.omg.CORBA.Object, org.omg.CORBA.portable.IDLEntity{
+    
+}
